b19dd9b80534dcb8ad17e6f05b5c4e86b234cb0a
Nara19941008 <narminim@code.edu.az>
2022-11-17 01:51:20 +0300

    Added bio table

---
+++ RepositoryLayer/Migrations/20221116224343_CreatedBookTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class CreatedBookTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Product",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 16, 22, 43, 43, 306, DateTimeKind.Utc).AddTicks(3188),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 16, 5, 34, 20, 789, DateTimeKind.Utc).AddTicks(6092));
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PageCount = table.Column<int>(type: "int", nullable: false),
+                    SoftDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2022, 11, 16, 22, 43, 43, 307, DateTimeKind.Utc).AddTicks(6874))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Product",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 16, 5, 34, 20, 789, DateTimeKind.Utc).AddTicks(6092),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 16, 22, 43, 43, 306, DateTimeKind.Utc).AddTicks(3188));
+        }
+    }
+}
---
+++ ServiceLayer/Services/Interfaces/IBookService.cs
@@ -0,0 +1,22 @@
+﻿using DomainLayer.Entities;
+using ServiceLayer.DTOs.Book;
+using ServiceLayer.DTOs.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services.Interfaces
+{
+    public interface IBookService
+    {
+        Task CreateAsync(BookCreateDto book); 
+        Task UpdateAsync(int id, BookUpdateDto book); 
+        Task DeleteAsync(int id); 
+        Task SoftDeleteAsync(int id);
+        Task<List<BookListDto>> GetAllAsync();
+        Task<Book> GetByIdAsync(int id);
+        Task<List<BookListDto>> SearchAsync(string? searchText);     
+    }
+}
